4624ec22d91f649bf2d679aac9ece1d229b50a73
ChenYiNong <chenyn2025@lzu.edu.cn>
2025-11-27 19:52:02 +0800

    function comlpeted,readme recovered

--- score_management.c
+++ score_management.c
@@ -64,19 +64,17 @@ void add_student(Student stu[],int* n,Student newdata)
 {
     for (size_t i = 0; i < *n; i++)
     {
-        if (stu[i].id==newdata.id)
+        if (strcmp(stu[i].id,newdata.id)==0)
         {
             printf("Duplicate student ID detected,operation denied\n");
             return;
         }
     }
     stu[*n]=newdata;
-    *n++;
-    free(n);
-    n=NULL;
+    (*n)++;
 }
 
-void deleta_student(Student stu[],int *n,
+void delete_student(Student stu[],int *n,
     char a[])
 {
     int t=-1;
@@ -92,8 +90,6 @@ void deleta_student(Student stu[],int *n,
     if (t==-1)
     {
         printf("The specified student cannot be found\n");
-        free(n);
-        n=NULL;
         return;
     }
     
@@ -102,8 +98,100 @@ void deleta_student(Student stu[],int *n,
     {
         stu[i]=stu[i+1];
     }
-    *n--;
-    free(n);
-    n=NULL;
+    (*n)--;
 }
 
+double score_get(Student stu,
+    int s)
+{
+    //1-math 2-English 3-moral 4-total score
+    if (s==1)
+    {
+        return stu.math;
+    }
+    else if (s==2)
+    {
+        return stu.english;
+    }
+    else if (s==3)
+    {
+        return stu.moral;
+    }
+    else if (s==4)
+    {
+        return stu.math+stu.english+stu.moral;
+    }
+    else
+    {
+        printf("Required subject does not exist\n");
+        return 0.0;
+    }
+}
+
+void sort_students(Student stu[],int n,
+    int subject)
+{
+    //1-math 2-English 3-moral 4-total score
+    Student sorted_stu[n],t=(Student){0};
+    for (size_t i = 0; i < n; i++)
+    {
+        sorted_stu[i]=stu[i];
+    }
+    for (size_t i = 0; i < n-1; i++)
+    {
+        for (size_t j = 0; j < n-1; j++)
+        {
+            if (score_get(sorted_stu[j],subject)
+            <score_get(sorted_stu[j+1],subject))
+            {
+                t=sorted_stu[j];
+                sorted_stu[j]=sorted_stu[j+1];
+                sorted_stu[j+1]=t;
+            }
+            
+        }
+    }
+    print_students(sorted_stu,n);
+}
+
+void search_by_id(Student stu[],int n,
+    char id[])
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        if (strcmp(stu[i].id,id)==0)
+        {
+            printf("ID        Name      Math   English  Moral\n");
+            printf("%s %s %lf %lf %lf\n",
+            stu[i].id,stu[i].name,
+            stu[i].math,stu[i].english,
+            stu[i].moral);
+            return;
+        }
+        
+    }
+    printf("Not found\n");
+}
+
+void search_by_name(Student stu[],int n,
+    char name[])
+{
+    int t=0;//是否找到
+    for (size_t i = 0; i < n; i++)
+    {
+        if (strcmp(stu[i].name,name)==0)
+        {
+            printf("ID        Name      Math   English  Moral\n");
+            printf("%s %s %lf %lf %lf\n",
+            stu[i].id,stu[i].name,
+            stu[i].math,stu[i].english,
+            stu[i].moral);
+            t=1;
+        }
+        
+    }
+    if(t==0)
+    {
+        printf("Not found\n");
+    }
+}
--- score_management.h
+++ score_management.h
@@ -18,4 +18,19 @@ void print_students(Student stu[],int n);
 
 void add_student(Student stu[],int* n,Student newdata);
 
+void delete_student(Student stu[],int *n,
+    char a[]);
+
+double score_get(Student stu,
+    int subject);
+
+void sort_students(Student stu[],int n,
+    int subject);
+
+void search_by_id(Student stu[],int n,
+    char id[]);
+
+void search_by_name(Student stu[],int n,
+    char name[]);
+
 #endif//一上来就写好了
